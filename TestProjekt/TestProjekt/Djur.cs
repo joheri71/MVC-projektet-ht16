bb9ac84763318a56ca424f2d94e843010be9fd9d
Daniel <daniel_toll@hotmail.com>
2016-12-21 10:06:20 +0100

    Update Djur.cs
    nytt namn på prop

--- TestProjekt/TestProjekt/Djur.cs
+++ TestProjekt/TestProjekt/Djur.cs
@@ -10,6 +10,6 @@ namespace TestProjekt
     {
         public int MittDjur { get; set; }
         public string Namn { get; set; }
-        public int Age { get; set; }
+        public int Ålder { get; set; }
     }
 }
