84ea78fb5e36605eb3a31aad7b1312c162a071b4
Daniel Toll <daniel_toll@hotmail.com>
2016-12-21 09:48:09 +0100

    La till en Djurklass

---
+++ TestProjekt/TestProjekt/Djur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjekt
+{
+    class Djur
+    {
+        public int MittDjur { get; set; }
+    }
+}
